598aa8bf6dcf41586c9104673ec38ee2ca24996b
맛카치 <mkachi@naver.com>
2018-05-28 15:47:09 +0900

    Fix translatesAutoresizingMaskIntoConstraints options

--- PureCode/PureCode/ViewController.swift
+++ PureCode/PureCode/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     var _nameBox : UITextField!
     var _messageLabel : UILabel!
+    var _button : UIButton!
+    var _nameLabel : UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,83 +32,51 @@ class ViewController: UIViewController {
     
     func setUpUI()
     {
-        let margins = view.layoutMarginsGuide
+        _nameLabel = UILabel(frame: CGRect(x: 83, y: 324.5, width: 59, height: 21))
+        _nameLabel.text = "Name : "
+        view.addSubview(_nameLabel)
         
-        let nameLabel = UILabel(frame: CGRect(x: 83, y: 324.5, width: 59, height: 21))
-        nameLabel.text = "Name : "
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.trailingAnchor.constraint(
-            equalTo: margins.trailingAnchor
-        ).isActive = true
-        nameLabel.leadingAnchor.constraint(
-            equalTo: margins.leadingAnchor,
-            constant: 83
-        ).isActive = true
-        nameLabel.topAnchor.constraint(
-            equalTo: margins.topAnchor,
-            constant: 14
-        ).isActive = true
-        view.addSubview(nameLabel)
-        
-        let button = UIButton(frame: CGRect(x: 164.5, y: 356.6, width: 55, height: 30))
-        button.setTitle("Button", for: UIControlState.normal)
-        button.setTitleColor(UIColor.blue, for: UIControlState.normal)
-        button.centerXAnchor.constraint(
-            equalTo: _messageLabel.centerXAnchor
-        ).isActive = true
-        button.topAnchor.constraint(
-            equalTo: _nameBox.bottomAnchor
-        ).isActive = true
-        view.addSubview(button)
-        
-        button.addTarget(self, action: #selector(self.button_touchDown(_:)), for: UIControlEvents.touchDown)
+        _button = UIButton(frame: CGRect(x: 164.5, y: 356.6, width: 55, height: 30))
+        _button.setTitle("Button", for: UIControlState.normal)
+        _button.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        _button.addTarget(self, action: #selector(self.button_touchDown(_:)), for: UIControlEvents.touchDown)
+        view.addSubview(_button)
         
         _nameBox = UITextField(frame: CGRect(x: 150, y: 318.5, width: 141, height: 30))
-        _nameBox.translatesAutoresizingMaskIntoConstraints = false
-        _nameBox.trailingAnchor.constraint(
-            equalTo: margins.trailingAnchor,
-            constant: 84
-        ).isActive = true
-        _nameBox.leadingAnchor.constraint(
-            equalTo: nameLabel.trailingAnchor
-        ).isActive = true
-        _nameBox.bottomAnchor.constraint(
-            equalTo: button.topAnchor
-        ).isActive = true
-        _nameBox.topAnchor.constraint(
-            equalTo: _messageLabel.bottomAnchor
-        ).isActive = true
-        _nameBox.centerYAnchor.constraint(
-            equalTo: margins.centerYAnchor,
-            constant: 0
-        ).isActive = true
+        _nameBox.borderStyle = UITextBorderStyle.roundedRect
         view.addSubview(_nameBox)
         
         _messageLabel = UILabel(frame: CGRect(x: 10, y: 289.5, width: 355, height: 21))
         _messageLabel.textAlignment = NSTextAlignment.center
         _messageLabel.text = "Hello, I am PureCode"
-        _messageLabel.centerXAnchor.constraint(
-            equalTo: margins.centerXAnchor
-        ).isActive = true
-        _messageLabel.centerXAnchor.constraint(
-            equalTo: button.centerXAnchor
-        ).isActive = true
-        _messageLabel.trailingAnchor.constraint(
-            equalTo: margins.trailingAnchor,
-            constant: 10
-        ).isActive = true
-        _messageLabel.leadingAnchor.constraint(
-            equalTo: margins.leadingAnchor,
-            constant: 10
-        ).isActive = true
-        _messageLabel.bottomAnchor.constraint(
-            equalTo: nameLabel.topAnchor,
-            constant: 14
-        ).isActive = true
-        _messageLabel.bottomAnchor.constraint(
-            equalTo: _nameBox.topAnchor
-        ).isActive = true
         view.addSubview(_messageLabel)
+        
+        constraints(margins: view.layoutMarginsGuide);
+    }
+    
+    func constraints(margins : UILayoutGuide)
+    {
+        _messageLabel.centerXAnchor.constraint(equalTo: margins.centerXAnchor, constant: 0).isActive = true
+        _messageLabel.centerXAnchor.constraint(equalTo: _button.centerXAnchor, constant: 0).isActive = true
+        _messageLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 10).isActive = true
+        _messageLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 10).isActive = true
+        _messageLabel.bottomAnchor.constraint(equalTo: _nameLabel.topAnchor, constant: 14).isActive = true
+        _messageLabel.bottomAnchor.constraint(equalTo: _nameBox.topAnchor).isActive = true
+        
+        _nameBox.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: 84).isActive = true
+        _nameBox.leadingAnchor.constraint(equalTo: _nameLabel.trailingAnchor).isActive = true
+        _nameBox.bottomAnchor.constraint(equalTo: _button.topAnchor).isActive = true
+        _nameBox.topAnchor.constraint(equalTo: _messageLabel.bottomAnchor).isActive = true
+        _nameBox.centerYAnchor.constraint(equalTo: margins.centerYAnchor, constant: 0).isActive = true
+        
+        _nameLabel.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        _nameLabel.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 83).isActive = true
+        _nameLabel.topAnchor.constraint(equalTo: margins.topAnchor, constant: 14).isActive = true
+        
+        _button.centerXAnchor.constraint(equalTo: _messageLabel.centerXAnchor).isActive = true
+        _button.topAnchor.constraint(equalTo: _nameBox.bottomAnchor).isActive = true
+        
+        view.updateConstraints()
     }
     
     @objc func button_touchDown(_ sender : UIButton) {
