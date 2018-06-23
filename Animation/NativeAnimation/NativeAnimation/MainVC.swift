523bb0c93c6e4d92ec1f45d364172fbfda90459e
맛카치 <mkachi@naver.com>
2018-06-23 16:04:24 +0900

    Add Native Animation project

---
+++ Animation/NativeAnimation/NativeAnimation/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  MainVC.swift
+//  NativeAnimation
+//
+//  Created by 김동영 on 2018. 6. 23..
+//  Copyright © 2018년 김동영. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func uiViewAnimation_touchDown(_ sender: Any) {
+    }
+    @IBAction func timerAnimation_touchDown(_ sender: Any) {
+    }
+    @IBAction func imageViewAnimation_touchDown(_ sender: Any) {
+    }
+}
---
+++ Animation/NativeAnimation/NativeAnimation/UIViewAniVC.swift
@@ -0,0 +1,22 @@
+//
+//  UIViewAniVC.swift
+//  NativeAnimation
+//
+//  Created by 김동영 on 2018. 6. 23..
+//  Copyright © 2018년 김동영. All rights reserved.
+//
+
+import UIKit
+
+class UIViewAniVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
